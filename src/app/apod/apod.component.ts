2231adf891f5e30ce333185aafb31a932f9f0c55
Christa Fuhrhop <christa.fuhrhop@gmail.com>
2019-04-29 12:29:39 -0500

    create a random date for each successful APOD

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-//1. Import ActivatedRoute
 import { ActivatedRoute } from '@angular/router';
 import { ApodService } from '../api/apod.service';
 import { Apod } from '../models/apod';
@@ -12,6 +11,8 @@ import { Apod } from '../models/apod';
 export class ApodComponent implements OnInit {
 
   apod:Apod;
+  //1. Create date as an instance variable
+  date:string;
 
   constructor(
     private apodService: ApodService,
@@ -29,14 +30,28 @@ export class ApodComponent implements OnInit {
   //4. Replace the current date with an updated method signature
   getApod(date:string): void{
   
-    this.apodService.getApod(date).subscribe(
-      (response:any)=>{
-        this.apod = response;
+  //If the date is falsy, use today's date
+  if(!date){
+    date = new Date().toISOString().slice(0,10);
+  }
 
-        //5. Log the results to the JS console
-        console.log(response);
-      }
+  this.apodService.getApod(date).subscribe(
+    (response:any)=>{
+      this.apod = response;
+      this.date = this.randomDate(new Date(1995,5,16), new Date());
+      console.log(this.apod);
+    }
+  );
+  }
+
+  //2. Create a method that returns a random date
+  randomDate(start, end): string{
+    let date = new Date(
+      start.getTime() + Math.random() *
+        (end.getTime() - start.getTime())
     );
+
+    return new Date(date).toISOString().slice(0,10);
   }
 
 }
